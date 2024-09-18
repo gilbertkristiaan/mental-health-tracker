f84e222695e65fbd4cbb392efe9d72b794abfd54
gilbertkristiaan <gilbertkristian79@gmail.com>
2024-09-18 15:54:31 +0700

    tutor 3 selesai

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mental_health_tracker.wsgi.application'
 
 
-# Database
+## Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
